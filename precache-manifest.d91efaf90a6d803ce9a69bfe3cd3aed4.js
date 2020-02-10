bf28cd009f2d4da736f3fa1f40f588659342a6f3
asenchekov <36127650+asenchekov@users.noreply.github.com>
2020-02-10 23:18:20 +0000

    Build for deploy

---
+++ precache-manifest.d91efaf90a6d803ce9a69bfe3cd3aed4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48951d109257e40e13bbd2dbc851b689",
+    "url": "/index.html"
+  },
+  {
+    "revision": "632ca9e50b1b99e30c8e",
+    "url": "/static/css/main.53c9da4b.chunk.css"
+  },
+  {
+    "revision": "df8dc5fed9576176eea7",
+    "url": "/static/js/0.121dc9e2.chunk.js"
+  },
+  {
+    "revision": "09eb58bf625c1519c198",
+    "url": "/static/js/3.0645de7b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/3.0645de7b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "632ca9e50b1b99e30c8e",
+    "url": "/static/js/main.c1b701e5.chunk.js"
+  },
+  {
+    "revision": "6d9822c200ec9cabe111",
+    "url": "/static/js/runtime-main.b0e723a1.js"
+  }
+]);
