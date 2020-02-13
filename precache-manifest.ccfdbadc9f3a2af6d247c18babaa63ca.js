6a1baebb887e002b10a5c543e2daee97c817d517
Asen Chekov <asenchekov@Users-MacBook-Pro.local>
2020-02-13 10:21:46 +0000

    Updates

---
+++ precache-manifest.ccfdbadc9f3a2af6d247c18babaa63ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4727be7424232c96036100450db4654b",
+    "url": "/favorite-rick-and-morty-episodes/index.html"
+  },
+  {
+    "revision": "9a622f95190ec8d43cee",
+    "url": "/favorite-rick-and-morty-episodes/static/css/main.54a1eda5.chunk.css"
+  },
+  {
+    "revision": "df8dc5fed9576176eea7",
+    "url": "/favorite-rick-and-morty-episodes/static/js/0.121dc9e2.chunk.js"
+  },
+  {
+    "revision": "09eb58bf625c1519c198",
+    "url": "/favorite-rick-and-morty-episodes/static/js/3.0645de7b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/favorite-rick-and-morty-episodes/static/js/3.0645de7b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a622f95190ec8d43cee",
+    "url": "/favorite-rick-and-morty-episodes/static/js/main.179bac20.chunk.js"
+  },
+  {
+    "revision": "fdade268f8e511deb06f",
+    "url": "/favorite-rick-and-morty-episodes/static/js/runtime-main.a71083ae.js"
+  }
+]);
