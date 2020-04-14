eee95e88c8fda3a2187a169dc545951732061673
Asen Chekov <asenchekov@gmail.com>
2020-04-14 21:43:47 +0100

    Updates

---
+++ precache-manifest.b0bfad0cb093f0c336897505fc8124ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e3c08eec5d724e10c3fd0d11c20a38",
+    "url": "/favorite-rick-and-morty-episodes/index.html"
+  },
+  {
+    "revision": "63f41aabf93e2734e7d1",
+    "url": "/favorite-rick-and-morty-episodes/static/css/main.54a1eda5.chunk.css"
+  },
+  {
+    "revision": "80a878b7b9ddd8122812",
+    "url": "/favorite-rick-and-morty-episodes/static/js/0.0c69b9ea.chunk.js"
+  },
+  {
+    "revision": "09eb58bf625c1519c198",
+    "url": "/favorite-rick-and-morty-episodes/static/js/3.0645de7b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/favorite-rick-and-morty-episodes/static/js/3.0645de7b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63f41aabf93e2734e7d1",
+    "url": "/favorite-rick-and-morty-episodes/static/js/main.f74b497c.chunk.js"
+  },
+  {
+    "revision": "a7ff10a1621f9b2c2e54",
+    "url": "/favorite-rick-and-morty-episodes/static/js/runtime-main.79a7441b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac617337acfcbf69bf9e369dc80e1256.js"
+  "/favorite-rick-and-morty-episodes/precache-manifest.b0bfad0cb093f0c336897505fc8124ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/favorite-rick-and-morty-episodes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
